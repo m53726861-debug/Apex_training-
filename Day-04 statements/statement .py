262c351e07e48d6877936669a5873d557680abd8
maha <m53726861@gmail.com>
2026-09-25 11:42:53 +0530

    Update statement .py

--- Day-04 statements/statement .py
+++ Day-04 statements/statement .py
@@ -46,3 +46,15 @@ print("Good bye!")
 output
 
 b is greater than a
+
+
+The count is: 0
+The count is: 1
+The count is: 2
+The count is: 3
+The count is: 4
+The count is: 5
+The count is: 6
+The count is: 7
+The count is: 8
+Good bye!
